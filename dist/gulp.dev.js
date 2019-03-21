9e5ecc5965f5fe1c0c00843a9454a6dd17645710
li2271587616 <2271587616@qq.com>
2019-03-21 15:08:17 +0800

    'gulpfile'

---
+++ dist/gulp.dev.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+ var connect = require('gulp-connect');
+
+gulp.task('default',['allFile','server','watch']);
+
+gulp.task('allFile',function(){
+     gulp.src('app/**/*')
+     .pipe(gulp.dest('dist'))
+     .pipe(connect,reload());
+});
+gulp.task('watch',function(){
+    gulp.watch('app/**/*',['allfile']);
+
+});
+
+gulp.task('server',function(){   //创建一个叫server的任务
+    connect.server({
+        root:'dist',  //设置根目录
+        livereload: true,   //是否热更新
+        port:8888
+    });
+
+})
